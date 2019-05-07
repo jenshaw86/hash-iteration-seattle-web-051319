702d61a5be788c65e94d54e0969cc7e6059600c4
Jenny Shaw <bloo.j.shaw@gmail.com>
2019-05-07 22:05:37 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,9 +5,9 @@ birthday_kids = {
 }
 
 def happy_birthday(birthday_kids)
-  birthday_kids.each do |name, age|
+  birthday_kids.each do |kids_name, age|
     if age <= 12
-      puts "Happy Birthday #{name}! You are now #{age} years old!"
+      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
     end
   end
 end
