63a3e395df52e2adf0a60bd7ea457ae84b711e7c
Jenny Shaw <bloo.j.shaw@gmail.com>
2019-05-07 21:38:46 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -3,7 +3,7 @@ passengers = {
   suite_b: "Seymour Hoffman", 
   suite_c: "Alfred Tennyson", 
   suite_d: "Charlie Chaplin", 
-suite_e: "Crumpet the Elf"
+  suite_e: "Crumpet the Elf"
 }
 
 def select_winner(passengers)
