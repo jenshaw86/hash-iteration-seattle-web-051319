c93dfaaf2589b01fbbfd68bd9d0c6e681023d75e
Jenny Shaw <bloo.j.shaw@gmail.com>
2019-05-07 22:09:05 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -9,7 +9,7 @@ def happy_birthday(birthday_kids)
     if age <= 12
       puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
     else 
-      put "You are too old for this."
+      puts "You are too old for this."
     end
   end
 end
